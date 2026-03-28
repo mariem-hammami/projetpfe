eb773e0d47e8eb3e7bb9f6849ba746ab40ef09d6
Oussama Zmitri <oussamazmitri123@gmail.com>
2026-03-28 17:40:35 +0100

    Add change_password endpoint and hash password verification

--- srt_bizerte_internal_system/backend/services/auth.py
+++ srt_bizerte_internal_system/backend/services/auth.py
@@ -1,20 +1,59 @@
 from flask import Blueprint, request, jsonify
-from db.database import execute_fetchone
+from werkzeug.security import check_password_hash, generate_password_hash
+from db.database import execute_fetchone, execute_modifi
 
 auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.json
+    user_id = data.get("user_id")
     username = data.get('username')
+    role = data.get('role')
     password = data.get('password')
+    if not (user_id and username and role and password):
+            return jsonify({"error": "missing information"}),400
+    try:
+        query = "SELECT id, username, role, password FROM users WHERE id = %s AND username=%s AND role =%s"
+        user = execute_fetchone(query, (user_id, username, role))
+        if not user:
+            return ({"error":"user not found please check the information you entered"}),404
+        hash_password = user[3]
+        if not check_password_hash(hash_password, password):
+            return jsonify({"message": "Invalid credentials"}), 401
 
+        return jsonify({"message": "Login successful", "user": user}),200
+    except Exception as e:
+        return jsonify({"error":str(e)})
    
-    query = "SELECT * FROM users WHERE username=%s AND password=%s"
-    user = execute_fetchone(query, (username, password))
+        
 
-    if user:
-        return jsonify({"message": "Login successful", "user": user})
-    else:
-        return jsonify({"message": "Invalid credentials"}), 401
+@auth_bp.route('/change_password', methods=['POST'])
+def change_password():
+    data = request.json
+    user_id = data.get("user_id")
+    username = data.get("username")
+    role = data.get("role")
+    old_password = data.get("old_password")
+    new_password = data.get("new_password")
+    try:
+        if not (user_id and username and role and old_password and new_password):
+            return jsonify({"error": "missing information"}),400
+        query = "SELECT password FROM users WHERE id = %s and username = %s and  role = %s"
+        params = (user_id, username, role)
+        password = execute_fetchone(query, params = params)
+        if not password:
+            return ({"error":"user not found please check the information you entered"}),404
+        if not check_password_hash(password[0], old_password):
+            return jsonify({"error":"please check your password"}), 401
+        hash_password = generate_password_hash(new_password)
+        query = "UPDATE users SET password = %s WHERE id = %s"
+        params = (hash_password, user_id)
+        execute_modifi(query, params = params)
+        return jsonify({"success":"password updated successfully"}), 200
+    except Exception as e:
+        return jsonify({"error":str(e)})
+    
+
+
 
